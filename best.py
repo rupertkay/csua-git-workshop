302ea1d242c5c73b01ed0389c91bb7e315e1db6a
Rupert Kay <Rupert Kay>
2017-09-08 18:39:16 -0700

    deleted everything lololol

--- best.py
+++ best.py
@@ -1,15 +1,3 @@
 
 def main():
-    person_a = "Rupert Kay"
-    superlative_a = "better"
-
-    person_b = "Mrunal Puram"
-    superlative_b = "good"
-
-    name = "Hilfinger"
-    position = "professor"
-    print("{0}: {1} is the {2} {3}!".format(person_a, name, superlative_a, position))
-    print("{0}: {1} is the {2} {3}!".format(person_b, name, superlative_b, position))
-
-if __name__ == '__main__':
-    main()
+	yolololololol
